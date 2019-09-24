711debfee9591318f9eb89df891f05f493584560
Marc224 <marcailagan21@gmail.com>
2019-09-24 15:42:44 -0700

    Fixed announce command

--- index.js
+++ index.js
@@ -12,40 +12,35 @@ client.on("ready", () => {
 				name: "me getting developed",
 				type: "STREAMING"
 			}
-		}); 
-	})
-
+	}	); 
+});
 client.on("message", (message) => {  
-  
-  	let messageArray = message.content.split(/\s+/g);
-  	let info = messageArray.slice(1).join(' ');
+	const args = message.content.slice(prefix.length).trim().split(/ +/g);
+	const command = args.shift().toLowerCase();
+	const cmd = message.content;
+	const user = message.mentions.users.first();
 
-  	const args = message.content.slice(prefix.length).trim().split(/ +/g);
-	const cmd = args.shift().toLowerCase();
-
-	if (!message.content.startsWith(prefix)) return;
+	if (!cmd.startsWith(prefix)) return;
  
-  	if (message.content.startsWith(prefix +'embed')) {
+  	if (cmd.startsWith(prefix +'embed')) {
     	message.channel.send(exampleEmbed);
     	console.log(message.author.username + " has sucessfully ran the embed command!");
   	}
  
-  	if (message.content.startsWith(prefix +'braden')) {
+  	if (cmd.startsWith(prefix +'braden')) {
     	message.channel.send("Braden Mateus Won Prom Court 6 Times!");
   	}
  
- 	 if (message.content.includes("nigga")) {
-    	message.channel.send("Homie really :(");
+ 	if (cmd.includes("nigga")) {
+		message.delete();
+    	message.reply(" said the N Word, go bully them!");
   	}
  
   	if (message.content.startsWith(prefix +'announce')) {
-		if (cmd.length = 0) {
-			message.channel.send("You need a message in order to announce smh.");
-		} else {
-			// Will add code later to this
-			return;
-		}
-  	}
+		message.delete();
+		let argsresult = args.join(" ");
+		message.channel.send(argsresult);
+	}
 });
 
 // This token is safely stored on my pc :) 
